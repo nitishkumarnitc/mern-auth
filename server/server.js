db21862fed71a98a50efb835e5ec2959cbafe406
nitishkumarnitc <nitishkumarnitc@gmail.com>
2020-01-03 10:54:45 +0530

    node_modules_merged

--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
 const users = require("./routes/api/users");
-
 const app = express();
-
-// Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
